1b29a1a0017c9750c5083ad2de5074a3017f5527
Hough <Caroline.Hough@dhsc.gov.uk>
2023-04-27 22:18:48 +0100

    Added documentation using Roxygen for get_latest_ons_data_url and download_latest_ons_data functions
    Updated filepath for metadata script

--- R/main.R
+++ R/main.R
@@ -4,7 +4,7 @@ if (!requireNamespace("librarian")) install.packages("librarian")
 librarian::shelf(rvest, dplyr, tidyverse, readxl)
 
 # source metadata functions
-source("src/get_ons_metadata.r")
+source("R/metadata.R")
 
 #retrieve metadata
 get_metadata(ons_url)
@@ -14,6 +14,16 @@ get_available_editions(ons_url)
 # Searches the ons_url for any xlsx links, turns relative links to absolute,
 # then filters to the first (assuming most recent) link
 
+#' Finds the first (assuming most recent) data (xlsx) URL on an ONS webpage
+#'
+#' @param ons_url The URL for the ONS webpage which contains embedded dataset link/s.
+#'
+#' @return data_url - the first, assuming most recent, data URL on the webpage
+#' @export
+#'
+#' @examples
+#' ons_url <- "https://www.ons.gov.uk/peoplepopulationandcommunity/healthandsocialcare/conditionsanddiseases/datasets/coronaviruscovid19infectionsurveydata"
+#' get_latest_ons_data_url(ons_url)
 get_latest_ons_data_url <- function(ons_url) {
   data_url <- read_html(ons_url) %>%
     html_elements("a") %>%
@@ -27,6 +37,18 @@ get_latest_ons_data_url <- function(ons_url) {
 # function to download latest data to destination location
 # (need to specify mode as wb(write binary) otherwise it will try to make the file compatible with Windows)
 
+#' Downloads first (assuming most recent) dataset link embedded on ONS webpage
+#'
+#' @param ons_url The URL for the ONS webpage which contains embedded dataset link/s.
+#' @param destfile The destination location where you want to save the file.
+#'
+#' @return The destination file path
+#' @export
+#'
+#' @examples
+#' ons_url <- "https://www.ons.gov.uk/peoplepopulationandcommunity/healthandsocialcare/conditionsanddiseases/datasets/coronaviruscovid19infectionsurveydata"
+#' destfile <- "data/cisdata.xlsx"
+#' download_latest_ons_data(ons_url, destfile)
 download_latest_ons_data <- function(ons_url, destfile) {
   data_url <- get_latest_ons_data_url(ons_url)
   download.file(data_url, destfile, mode="wb")
