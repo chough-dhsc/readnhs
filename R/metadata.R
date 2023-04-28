be1d401b0d9568957502d369b1bd3900dafad6ef
Hough <Caroline.Hough@dhsc.gov.uk>
2023-04-28 12:41:56 +0100

    Used Roxygen to add documentation for each metadata function and then ran devtools::document() to generate .Rd files for each function

--- R/metadata.R
+++ R/metadata.R
@@ -4,6 +4,21 @@ library(rvest)
 # functions to extract relevant metadata - title, release date, next updated, about this dataset, editions available
 
 #generic function for retrieving html text
+#' Retrieves html text
+#'
+#' @param ons_url The ONS URL from which you want to read text.
+#' @param element_info The html element you want to read.
+#'
+#' @return html_text_script The text from the element you have specified.
+#' @export
+#'
+#' @examples
+#' ons_url <- "https://www.ons.gov.uk/peoplepopulationandcommunity/healthandsocialcare/conditionsanddiseases/datasets/coronaviruscovid19infectionsurveydata"
+#'
+#' element_info <- "h1.page-intro__title"
+#'
+#' get_html_text(ons_url, element_info)
+
 get_html_text <- function(ons_url, element_info){
   html_text_script <- read_html(ons_url) %>%
     html_elements(element_info) %>%
@@ -12,21 +27,69 @@ get_html_text <- function(ons_url, element_info){
 }
 
 #use generic function for retrieving html text within other get metadata functions
+#' Retrieves dataset title from ONS webpage
+#'
+#' @param ons_url The ONS URL which contains the embedded dataset link.
+#'
+#' @return dataset_title The title of the dataset.
+#' @export
+#'
+#' @examples
+#' ons_url <- "https://www.ons.gov.uk/peoplepopulationandcommunity/healthandsocialcare/conditionsanddiseases/datasets/coronaviruscovid19infectionsurveydata"
+#'
+#' get_dataset_title(ons_url)
+#'
 get_dataset_title <-function(ons_url){
   dataset_title <- get_html_text(ons_url,"h1.page-intro__title") %>%
     str_replace("Dataset ","")
   return(dataset_title)
 }
 
+#' Retrieves the release date of the latest dataset available
+#'
+#' @param ons_url The ONS URL which contains the embedded dataset link.
+#'
+#' @return release_date The release date of the latest dataset available.
+#' @export
+#'
+#' @examples
+#' ons_url <- "https://www.ons.gov.uk/peoplepopulationandcommunity/healthandsocialcare/conditionsanddiseases/datasets/coronaviruscovid19infectionsurveydata"
+#'
+#' get_release_date(ons_url)
+
 get_release_date <- function(ons_url) {
   release_date <- get_html_text(ons_url,".meta__item:nth-child(2) .meta__term+ div")
   return(release_date)
 }
 
+#' Retrieves the date the next version of the dataset will be made available
+#'
+#' @param ons_url The ONS URL which contains the embedded dataset link.
+#'
+#' @return next_update The date the next version of the dataset will be made available
+#' @export
+#'
+#' @examples
+#' ons_url <- "https://www.ons.gov.uk/peoplepopulationandcommunity/healthandsocialcare/conditionsanddiseases/datasets/coronaviruscovid19infectionsurveydata"
+#'
+#' get_next_updated(ons_url)
+
 get_next_updated <- function(ons_url) {
   next_update <- get_html_text(ons_url,".meta__item~ .meta__item+ .meta__item .meta__term+ div")
   return(next_update)
 }
+
+#' Retrieves additional information about this dataset
+#'
+#' @param ons_url The ONS URL which contains the embedded dataset link.
+#'
+#' @return about_this dataset Additional information provided by ONS about this dataset.
+#' @export
+#'
+#' @examples
+#' ons_url <- "https://www.ons.gov.uk/peoplepopulationandcommunity/healthandsocialcare/conditionsanddiseases/datasets/coronaviruscovid19infectionsurveydata"
+#'
+#' get_about_this_dataset(ons_url)
 
 get_about_this_dataset <- function(ons_url){
   about_this_dataset <- get_html_text(ons_url, "#main section:nth-child(1)") %>%
@@ -34,11 +97,35 @@ get_about_this_dataset <- function(ons_url){
   return(about_this_dataset)
 }
 
+#' Retrieves a list of all available editions of the dataset
+#'
+#' @param ons_url The ONS URL which contains the embedded dataset link.
+#'
+#' @return available_editions A list of all available editions of the dataset.
+#' @export
+#'
+#' @examples
+#' ons_url <- "https://www.ons.gov.uk/peoplepopulationandcommunity/healthandsocialcare/conditionsanddiseases/datasets/coronaviruscovid19infectionsurveydata"
+#'
+#' get_available_editions(ons_url)
+
 get_available_editions <- function(ons_url){
   available_editions <- get_html_text(ons_url, "h3") %>%
     str_subset("edition")
   return(available_editions)
 }
+
+#' Retrieves the latest(first listed) edition name of the dataset
+#'
+#' @param ons_url The ONS URL which contains the embedded dataset link.
+#'
+#' @return latest_edition The latest edition name of the dataset
+#' @export
+#'
+#' @examples
+#' ons_url <- "https://www.ons.gov.uk/peoplepopulationandcommunity/healthandsocialcare/conditionsanddiseases/datasets/coronaviruscovid19infectionsurveydata"
+#'
+#' get_latest_edition(ons_url)
 
 get_latest_edition <- function(ons_url){
   latest_edition <- get_available_editions(ons_url) %>%
@@ -47,6 +134,18 @@ get_latest_edition <- function(ons_url){
 }
 
 # Higher level function that runs all functions above to get relevant metadata and return metadata as list with each element named properly
+#' Higher level function that retrieves relevant metadata for ONS datasets
+#'
+#' @param ons_url The ONS URL which contains the embedded dataset link.
+#'
+#' @return metadata A list containing the dataset title, latest edition, release date, when it will next be updated and additional information about the dataset.
+#' @export
+#'
+#' @examples
+#' ons_url <- "https://www.ons.gov.uk/peoplepopulationandcommunity/healthandsocialcare/conditionsanddiseases/datasets/coronaviruscovid19infectionsurveydata"
+#'
+#' get_metadata(ons_url)
+
 get_metadata <- function(ons_url){
 
   metadata <- list(
