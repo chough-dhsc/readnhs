62d762c912e2198a95fbc8cd234a138e4cf6d8ba
Hough <Caroline.Hough@dhsc.gov.uk>
2023-06-07 13:13:06 +0100

    added roxygen importFrom and added the library references that each imported function belongs to. Also split example urls over multiple lines to prevent them being truncated

--- R/getmetadata.R
+++ R/getmetadata.R
@@ -7,19 +7,25 @@
 #' @param element_info The html element you want to read.
 #'
 #' @return html_text_script The text from the element you have specified.
+#'
+#' @importFrom rvest read_html
+#' @importFrom rvest html_elements
+#' @importFrom rvest html_text2
 #' @export
 #'
 #' @examples
-#' ons_url <- "https://www.ons.gov.uk/peoplepopulationandcommunity/healthandsocialcare/conditionsanddiseases/datasets/coronaviruscovid19infectionsurveydata"
+#' ons_url <- paste("https://www.ons.gov.uk/peoplepopulationandcommunity/",
+#' "healthandsocialcare/conditionsanddiseases/datasets/coronaviruscovid19",
+#' "infectionsurveydata", sep="")
 #'
 #' element_info <- "h1.page-intro__title"
 #'
 #' get_html_text(ons_url, element_info)
 
 get_html_text <- function(ons_url, element_info){
-  html_text_script <- read_html(ons_url) %>%
-    html_elements(element_info) %>%
-    html_text2()
+  html_text_script <- rvest::read_html(ons_url) %>%
+    rvest::html_elements(element_info) %>%
+    rvest::html_text2()
   return(html_text_script)
 }
 
@@ -29,16 +35,19 @@ get_html_text <- function(ons_url, element_info){
 #' @param ons_url The ONS URL which contains the embedded dataset link.
 #'
 #' @return dataset_title The title of the dataset.
+#' @importFrom stringr str_replace
 #' @export
 #'
 #' @examples
-#' ons_url <- "https://www.ons.gov.uk/peoplepopulationandcommunity/healthandsocialcare/conditionsanddiseases/datasets/coronaviruscovid19infectionsurveydata"
+#' ons_url <- paste("https://www.ons.gov.uk/peoplepopulationandcommunity/",
+#' "healthandsocialcare/conditionsanddiseases/datasets/coronaviruscovid19",
+#' "infectionsurveydata", sep="")
 #'
 #' get_dataset_title(ons_url)
 #'
 get_dataset_title <-function(ons_url){
   dataset_title <- get_html_text(ons_url,"h1.page-intro__title") %>%
-    str_replace("Dataset ","")
+    stringr::str_replace("Dataset ","")
   return(dataset_title)
 }
 
@@ -50,7 +59,9 @@ get_dataset_title <-function(ons_url){
 #' @export
 #'
 #' @examples
-#' ons_url <- "https://www.ons.gov.uk/peoplepopulationandcommunity/healthandsocialcare/conditionsanddiseases/datasets/coronaviruscovid19infectionsurveydata"
+#' ons_url <- paste("https://www.ons.gov.uk/peoplepopulationandcommunity/",
+#' "healthandsocialcare/conditionsanddiseases/datasets/coronaviruscovid19",
+#' "infectionsurveydata", sep="")
 #'
 #' get_release_date(ons_url)
 
@@ -67,7 +78,9 @@ get_release_date <- function(ons_url) {
 #' @export
 #'
 #' @examples
-#' ons_url <- "https://www.ons.gov.uk/peoplepopulationandcommunity/healthandsocialcare/conditionsanddiseases/datasets/coronaviruscovid19infectionsurveydata"
+#' ons_url <- paste("https://www.ons.gov.uk/peoplepopulationandcommunity/",
+#' "healthandsocialcare/conditionsanddiseases/datasets/coronaviruscovid19",
+#' "infectionsurveydata", sep="")
 #'
 #' get_next_updated(ons_url)
 
@@ -81,16 +94,19 @@ get_next_updated <- function(ons_url) {
 #' @param ons_url The ONS URL which contains the embedded dataset link.
 #'
 #' @return about_this dataset Additional information provided by ONS about this dataset.
+#' @importFrom stringr str_replace
 #' @export
 #'
 #' @examples
-#' ons_url <- "https://www.ons.gov.uk/peoplepopulationandcommunity/healthandsocialcare/conditionsanddiseases/datasets/coronaviruscovid19infectionsurveydata"
+#' ons_url <- paste("https://www.ons.gov.uk/peoplepopulationandcommunity/",
+#' "healthandsocialcare/conditionsanddiseases/datasets/coronaviruscovid19",
+#' "infectionsurveydata", sep="")
 #'
 #' get_about_this_dataset(ons_url)
 
 get_about_this_dataset <- function(ons_url){
   about_this_dataset <- get_html_text(ons_url, "#main section:nth-child(1)") %>%
-    str_replace("(.*?)\n","")
+    stringr::str_replace("(.*?)\n","")
   return(about_this_dataset)
 }
 
@@ -99,16 +115,19 @@ get_about_this_dataset <- function(ons_url){
 #' @param ons_url The ONS URL which contains the embedded dataset link.
 #'
 #' @return available_editions A list of all available editions of the dataset.
+#' @importFrom stringr str_subset
 #' @export
 #'
 #' @examples
-#' ons_url <- "https://www.ons.gov.uk/peoplepopulationandcommunity/healthandsocialcare/conditionsanddiseases/datasets/coronaviruscovid19infectionsurveydata"
+#' ons_url <- paste("https://www.ons.gov.uk/peoplepopulationandcommunity/",
+#' "healthandsocialcare/conditionsanddiseases/datasets/coronaviruscovid19",
+#' "infectionsurveydata", sep="")
 #'
 #' get_available_editions(ons_url)
 
 get_available_editions <- function(ons_url){
   available_editions <- get_html_text(ons_url, "h3") %>%
-    str_subset("edition")
+    stringr::str_subset("edition")
   return(available_editions)
 }
 
@@ -117,16 +136,19 @@ get_available_editions <- function(ons_url){
 #' @param ons_url The ONS URL which contains the embedded dataset link.
 #'
 #' @return latest_edition The latest edition name of the dataset
+#' @importFrom gdata first
 #' @export
 #'
 #' @examples
-#' ons_url <- "https://www.ons.gov.uk/peoplepopulationandcommunity/healthandsocialcare/conditionsanddiseases/datasets/coronaviruscovid19infectionsurveydata"
+#' ons_url <- paste("https://www.ons.gov.uk/peoplepopulationandcommunity/",
+#' "healthandsocialcare/conditionsanddiseases/datasets/coronaviruscovid19",
+#' "infectionsurveydata", sep="")
 #'
 #' get_latest_edition(ons_url)
 
 get_latest_edition <- function(ons_url){
   latest_edition <- get_available_editions(ons_url) %>%
-    first()
+    gdata::first()
   return(latest_edition)
 }
 
@@ -139,7 +161,9 @@ get_latest_edition <- function(ons_url){
 #' @export
 #'
 #' @examples
-#' ons_url <- "https://www.ons.gov.uk/peoplepopulationandcommunity/healthandsocialcare/conditionsanddiseases/datasets/coronaviruscovid19infectionsurveydata"
+#' ons_url <- paste("https://www.ons.gov.uk/peoplepopulationandcommunity/",
+#' "healthandsocialcare/conditionsanddiseases/datasets/coronaviruscovid19",
+#' "infectionsurveydata", sep="")
 #'
 #' get_metadata(ons_url)
 
