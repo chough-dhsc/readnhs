09ea760f90ecfe6b2ad574213006c07df546747c
Hough <Caroline.Hough@dhsc.gov.uk>
2023-07-19 16:01:06 +0100

    added tests to check valid and invalid urls generate correct output/error

--- tests/testthat/test-getmetadata.R
+++ tests/testthat/test-getmetadata.R
@@ -1,5 +1,31 @@
-test_that("multiplication works", {
-  expect_equal(2 * 2, 4)
+test_that("valid url generates right output", {
+  ons_url <- paste0(
+    "https://www.ons.gov.uk/peoplepopulationandcommunity/",
+    "healthandsocialcare/conditionsanddiseases/datasets/coronaviruscovid19",
+    "infectionsurveydata"
+  )
+
+  element_info <- "h1.page-intro__title"
+
+  df <- get_html_text(ons_url, element_info)
+
+  result <- "Dataset Coronavirus (COVID-19) Infection Survey: England"
+
+  expect_equal(df, result)
 })
 
+test_that("broken url generates error", {
+  ons_url <- paste0(
+    "https://ww.ons.gov.uk/peoplpopulationandcommunity/",
+    "healthandsocialcare/conditionsanddiseases/datasets/coronaviruscovid19",
+    "infectionsurveydata"
+  )
 
+  element_info <- "h1.page-intro__title"
+
+  df <- get_html_text(ons_url, element_info)
+
+  result <- "Invalid URL"
+
+  expect_equal(df, result)
+})
