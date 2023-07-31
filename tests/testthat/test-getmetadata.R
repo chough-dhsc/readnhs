ee4a032224ef3af625d985ad85ae2d9137cd39e1
Hough <Caroline.Hough@dhsc.gov.uk>
2023-07-31 17:41:36 +0100

    comments added based on catch up with MK with some additional tests to be developed

--- tests/testthat/test-getmetadata.R
+++ tests/testthat/test-getmetadata.R
@@ -26,13 +26,16 @@ test_that("broken url generates error", {
   expect_error(get_html_text(ons_url, element_info), "Invalid URL")
 })
 
-#Exited with status -1073741819
-# test_that("number parsed as ons_url generates error", {
-#   ons_url <- 999
-#
-#   element_info <- "h1.page-intro__title"
-#
-#   expect_error(get_html_text(ons_url, element_info), "Invalid URL")
-# })
 
-#how to test if html element input is invalid?
+test_that("number parsed as ons_url generates error", {
+  ons_url <- 999
+
+  element_info <- "h1.page-intro__title"
+
+  expect_error(get_html_text(ons_url, element_info), "Invalid input")
+})
+
+#what happens if you parse a vector of URLs?
+#how to test if html element input is invalid? - validate within function that its a single string not list
+#unit test for error/return empty string
+
