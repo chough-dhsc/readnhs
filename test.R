56760ea0eecdfc6252f11276e1a56b1fcb25c3c5
Hough <caroline.hough@dhsc.gov.uk>
2023-08-02 17:52:56 +0100

    deleted superfluous test file

--- test.R
+++
@@ -1,8 +0,0 @@
-
-devtools::load_all()
-
-ons_url <- "https://www.ons.gov.uk/peoplepopulationandcommunity/healthandsocialcare/conditionsanddiseases/datasets/coronaviruscovid19infectionsurveydata"
-
-#retrieve metadata
-get_metadata(ons_url)
-get_available_editions(ons_url)
