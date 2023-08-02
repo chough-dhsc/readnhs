18b87ad6d9dc04e1c2cd77171c996c1bff75aade
Hough <caroline.hough@dhsc.gov.uk>
2023-08-02 20:47:43 +0100

    amended testthat script to reflect new package name

--- tests/testthat.R
+++ tests/testthat.R
@@ -7,6 +7,6 @@
 # * https://testthat.r-lib.org/reference/test_package.html#special-files
 
 library(testthat)
-library(onswebscrapper)
+library(readons)
 
-test_check("onswebscrapper")
+test_check("readons")
